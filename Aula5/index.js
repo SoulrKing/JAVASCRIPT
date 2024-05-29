2cd5bc21c31a2164aeccaf5c3e5c264f7732fb2d
SoulrKing <conteudosroger@gmail.com>
2024-05-29 18:53:55 -0300

    aula 12 com exercicio feito

---
+++ Aula5/index.js
@@ -0,0 +1,5 @@
+var velo = 10
+console.log("A velocidade do carro é "+ velo +"km/h" )
+if(velo > 60){ // condição simples, não obtem senão.
+    console.log("Você ultrapassou a velocidade da via! Está multado!")
+}
