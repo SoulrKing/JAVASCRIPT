69b2e5e309fccfb1b53db45e82843b6c702dbba9
SoulrKing <conteudosroger@gmail.com>
2024-06-11 17:50:31 -0300

    exercio de tabuada ainda nao feito

---
+++ exercicios/exercicio_tabuada/script.js
@@ -0,0 +1,10 @@
+function gerar(){
+    var numero = document.getElementById("numero")
+    var resultado = document.getElementById("tabuada")
+    var calculo 
+    if(numero <= 0){
+        window.alert("Adicione um numero valido!")
+    }else{
+        numero * 
+    }
+}
