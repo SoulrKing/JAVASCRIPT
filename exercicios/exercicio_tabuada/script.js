57276808532bcc954dc56b0e4e2982f1f54ea377
SoulrKing <conteudosroger@gmail.com>
2024-06-12 12:56:11 -0300

    exercicio de tabuada criado

--- exercicios/exercicio_tabuada/script.js
+++ exercicios/exercicio_tabuada/script.js
@@ -1,10 +1,20 @@
 function gerar(){
     var numero = document.getElementById("numero")
-    var resultado = document.getElementById("tabuada")
+    var tabuada = document.getElementById("tabuada")
     var calculo 
-    if(numero <= 0){
-        window.alert("Adicione um numero valido!")
+    if(numero.value.length == 0){
+        window.alert("digite um numero valido")
+
     }else{
-        numero * 
+        let n = Number(numero.value)
+        let c = 1
+        tabuada.innerHTML = " "
+        while (c <= 10){
+            let item = document.createElement("option")
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `Tabuada${c}`
+            tabuada.appendChild(item)
+            c++
+        }
     }
 }
