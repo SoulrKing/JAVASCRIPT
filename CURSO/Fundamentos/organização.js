75c194f1c21319d57ff08e844a05e398b0f44e56
Roger Henrique <conteudosroger@gmail.com>
2024-07-26 14:14:33 -0300

    Iniciei o reestudo sobre javascript

---
+++ CURSO/Fundamentos/organização.js
@@ -0,0 +1,8 @@
+console.log("Sentença de código") // quebra de linha e má formação de codigo pode gerar erro.
+
+//codigos javaScript pode ser organizado por blocos de codigos também.
+
+{
+    console.log("olá")
+    console.log("Mundo!")
+}
