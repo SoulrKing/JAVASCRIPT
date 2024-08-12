bb9f66f4c166fe6219e4a1bdef57acb3082fcf57
Roger Henrique <conteudosroger@gmail.com>
2024-08-12 15:01:40 -0300

    Create arrowfunction.js

---
+++ CURSO/Funções/arrowfunction.js
@@ -0,0 +1,20 @@
+let dobro = function (a) {
+    return 2 * a
+}
+
+dobro = (a) => {
+    return 2 * a
+}
+
+dobro = a => 2 * a //return esta implicito
+
+console.log(dobro(Math.PI))
+
+let ola = function(){
+    return "olá!"
+}
+
+ola = () => "olá!"
+ola = _ => "olá" // Possui um parametro
+
+console.log(ola())
