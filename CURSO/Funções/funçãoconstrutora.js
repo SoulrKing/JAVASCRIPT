3aaeea3a0eeb1aa058f15bab051d848abb669cf5
Roger Henrique <conteudosroger@gmail.com>
2024-08-19 14:02:16 -0300

    Create funçãoconstrutora.js

---
+++ CURSO/Funções/funçãoconstrutora.js
@@ -0,0 +1,30 @@
+function carro(velocidadeMaxima = 200, delta = 5) {
+    //atributo privado
+    let velocidadeAtual = 0
+
+    //metodo publico
+    this.acelerar = function () {
+        if (velocidadeAtual + delta <= velocidadeMaxima) {
+            velocidadeAtual += delta
+        } else {
+            velocidadeAtual = velocidadeMaxima
+        }
+    }
+
+
+    //metodo publico 
+    this.getvelocidadeAtual = function () {
+        return velocidadeAtual
+    }
+}
+
+const uno = new carro
+uno.acelerar()
+console.log(uno.getvelocidadeAtual())
+
+const ferrari = new carro(350, 20)
+ferrari.acelerar()
+ferrari.acelerar()
+ferrari.acelerar()
+ferrari.acelerar()
+console.log(ferrari.getvelocidadeAtual())
